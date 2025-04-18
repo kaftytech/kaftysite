c8166618b7297bbf10e34ea0077f1f6d340dfe30
kafty <kaftytechnologies@gmail.com>
2025-04-19 02:56:06 +0530

    responsive

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const navLinks = document.getElementsByClassName("closing");
+const sideNav = document.getElementsByClassName("sideNav")[0];
+const menu = document.getElementsByClassName("menu")[0];
+const closeNav = document.getElementsByClassName("sideNavClose")[0];
+menu.addEventListener("click", () => {
+  sideNav.style.display = "block";
+});
+closeNav.addEventListener("click", () => {
+  sideNav.style.display = "none";
+});
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", () => {
+    sideNav.style.display = "none";
+  });
+}
+
 const scriptURL =
   "https://script.google.com/macros/s/AKfycbz7CRL3o6XS7rRQ11CHbSoHlhii6DxnRDH4zTMoqeluzGQ1XtlfHJ18h1hbzdaf4bpS/exec";
 const form = document.forms["google-sheet"];
